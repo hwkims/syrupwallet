a8267489d249db8f27d1efab3e20745b8cfb4e36
hwkims <hwkims@naver.com>
2024-06-21 12:56:53 +0900

    1

---
+++ js/syrup.js
@@ -0,0 +1,15 @@
+// 슬라이드 기능
+const sliders = document.querySelectorAll('.slider');
+sliders.forEach(slider => {
+    const prev = slider.querySelector('.prev');
+    const next = slider.querySelector('.next');
+    // ... 슬라이드 기능 구현
+});
+
+// 이미지 갤러리 기능
+const galleryImages = document.querySelectorAll('.gallery-image');
+// ... 이미지 갤러리 기능 구현
+
+// 맵 기능
+const map = document.getElementById('map');
+// ... 맵 기능 구현
